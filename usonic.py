cd31dfe209509b5736de099a007e995124dbfc52
Naohiro Tsuji <naohiro2g@gmail.com>
2016-05-03 17:09:07 +0900

    good one

--- usonic.py
+++ usonic.py
@@ -1,45 +1,32 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 # GPIO output (17pin) = "Trig" on the sensor
 # GPIO input (27pin) = "Echo" on the sensor
+
+import time
+import RPi.GPIO as GPIO
 
 TRIG = 17
 ECHO = 27
 
+# Disable any warning message such as GPIO pins in use
+GPIO.setwarnings(False)
+# to refer to the pins by the "Broadcom SOC channel" number
+GPIO.setmode(GPIO.BCM)
+
 def read_usonic(sensor):
-    import time
-    import RPi.GPIO as GPIO
-
-    # Disable any warning message such as GPIO pins in use
-
-    GPIO.setwarnings(False)
-
-    # to refer to the pins by the "Broadcom SOC channel" number
-
-    GPIO.setmode(GPIO.BCM)
-
     if sensor == 0:
-
         GPIO.setup(TRIG,GPIO.OUT)
         GPIO.setup(ECHO,GPIO.IN)
         GPIO.output(TRIG, GPIO.LOW)
 
-        # found that the sensor can crash if there isn't a delay here
-        # no idea why. If you have odd crashing issues, increase delay
+        time.sleep(0.5)
 
-        time.sleep(0.8)
-
-        # sensor manual says a pulse enough of 10us will trigger the
-        # sensor to transmit 8 cycles of ultrasonic burst at 40kHz and
-        # wait for the reflected ultrasonic burst to be received
-
-        # start the pulse
-        # wait 10 micro seconds (this is 0.00001 seconds)
-        # stop the pulse after the time above has passed
-
-        GPIO.output(TRIG, True)
+        # send a 10us pulse to Trigger
+        # It might be 180uS or longer in fact.
+        GPIO.output(TRIG, GPIO.HIGH)
         time.sleep(0.00001)
-        GPIO.output(TRIG, False)
+        GPIO.output(TRIG, GPIO.LOW)
 
         while GPIO.input(ECHO) == 0:
           signaloff = time.time()
@@ -47,7 +34,7 @@ def read_usonic(sensor):
           signalon = time.time()
         timepassed = signalon - signaloff
 
-        distance = timepassed * 34000 / 2     # 340 m/s
+        distance = timepassed * 17000     # 340 m/s / 2 in cm
         return distance
 
     else:
@@ -56,7 +43,8 @@ def read_usonic(sensor):
 
 try:
     while True:
-        print "\t distance to obstacle = ", round(read_usonic(0),1), "[cm]"
-        time.sleep(0.5)
+        print "\t distance = ", round(read_usonic(0),1), "[cm]"
+#        time.sleep(0.5)
 except KeyboardInterrupt:
     pass
+GPIO.cleanup()
